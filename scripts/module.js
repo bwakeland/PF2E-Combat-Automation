1924629c12ae8a6ced0249a7edff4e76569ffed0
Ben Wakeland <benjamin.wakeland@utsouthwestern.edu>
2021-03-06 20:28:46 -0600

    Fixed unreferenced variable errors

--- scripts/module.js
+++ scripts/module.js
@@ -8,7 +8,7 @@ Hooks.once('ready', async function() {
 });
 
 Hooks.on("createChatMessage", (message) => {
-   if((data.flags.pf2e.context.type === "spell-attack-roll") || (data.flags.pf2e.context.type === "attack-roll")) {
+   if((message.data.flags.pf2e.context.type === "spell-attack-roll") || (message.data.flags.pf2e.context.type === "attack-roll")) {
        parseAttack(message)
    }
 });
