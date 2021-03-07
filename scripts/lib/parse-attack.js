912f05f6e41f25db8ffeca677885bed089340fef
Ben Wakeland <benjamin.wakeland@utsouthwestern.edu>
2021-03-06 20:32:53 -0600

    Debugging

--- scripts/lib/parse-attack.js
+++ scripts/lib/parse-attack.js
@@ -72,11 +72,13 @@ export function parseAttack(message) {
     game.user.targets.forEach(attackTarget => {
         const rollTarget = attackTarget.actor.data.data.attributes.ac.value
         const degreeOfSuccess = calcDegreeOfSuccess(rollTarget, rollTotal);
+        console.log("Logging the attackTarget to find the name")
+        console.log(attackTarget)
         const attackResult = {
             rollTarget: rollTarget,
             degreeOfSuccess: degreeOfSuccess,
             attacker: message.user.data.name,
-            target: attackTarget.value.data.name
+            //target: attackTarget.value.data.name
         };
         attackResults.push(attackResult)
     });
