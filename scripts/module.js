d1d4733774cc30f6688050c0579c2a951e7b28b5
Ben Wakeland <benjamin.wakeland@utsouthwestern.edu>
2021-03-07 11:20:19 -0600

    Fixed error in non-attack chat messages

--- scripts/module.js
+++ scripts/module.js
@@ -1,14 +1,17 @@
 import {parseAttack} from "./lib/parse-attack.js";
-Hooks.once('init', async function() {
+
+Hooks.once('init', async function () {
 
 });
 
-Hooks.once('ready', async function() {
+Hooks.once('ready', async function () {
 
 });
 
 Hooks.on("createChatMessage", (message) => {
-   if((message.data.flags.pf2e.context.type === "spell-attack-roll") || (message.data.flags.pf2e.context.type === "attack-roll")) {
-       parseAttack(message)
-   }
+    if (message.data.flags.pf2e.context.type != 'undefined') {
+        if ((message.data.flags.pf2e.context.type === "spell-attack-roll") || (message.data.flags.pf2e.context.type === "attack-roll")) {
+            parseAttack(message)
+        }
+    }
 });
